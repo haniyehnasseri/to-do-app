b8c8025ae083f1cf1b611a0515988527f67c9515
Haniyeh Nasseri <haniyeh.nasseri99@gmail.com>
2020-12-13 19:02:14 +0330

    "Part 2"

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloModule } from './hello/hello.module';
+import { BooksModule } from './books/books.module';
+import { GenreModule } from './genre/genre.module';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import BookEntity from './db/entity/book.entity';
+import GenreEntity from './db/entity/genre.entity';
+import UserEntity from './db/entity/user.entity';
 
 @Module({
-  imports: [HelloModule],
+  imports: [HelloModule, BooksModule, GenreModule, UserModule,TypeOrmModule.forFeature([UserEntity, BookEntity , GenreEntity],),TypeOrmModule.forRoot(),],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/books/books.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Post, Body, Get } from '@nestjs/common';
+import {BooksService} from './books.service';
+import CreateBookDto from './dto/create-book.dto';
+
+@Controller('books')
+export class BooksController {
+    constructor(private readonly booksService: BooksService) {}
+    @Post('post')
+    postBook( @Body() book: CreateBookDto) {
+        return this.booksService.insert(book);
+    }
+
+    @Get()
+    getAll() {
+        return this.booksService.getAllBooks();
+    }
+
+}
