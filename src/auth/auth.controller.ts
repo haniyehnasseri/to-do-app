c8353f80d41047e581ae34e15eef6d1d4a0d7bd7
Haniyeh Nasseri <haniyeh.nasseri99@gmail.com>
2020-12-15 11:56:04 +0330

    Part4 - Swagger improvement

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,15 +5,24 @@ import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthGuard } from '@nestjs/passport'; 
 import { ApiBody, ApiResponse } from '@nestjs/swagger';
 import { type } from 'os';
+import { json } from 'express';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @ApiBody({type:'JSON'}) 
+
+
+  @ApiBody({schema : {properties : {username : {type: 'string', default: "Haniyeh"}, password : {type: 'string', default: "****"} }}}) 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
+
+    interface IDataNode {
+        id: number;
+        title: string;
+        node: Array<IDataNode>;
+    }
     
     return this.authService.login(req.user);
   }
